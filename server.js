b8892189e4a79574cc3250333eefce459498de47
Anas Gamal <agamal@stetson.edu>
2023-05-22 22:30:15 +0300

    Refactor: change directory name 'website' to 'src'

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('src'));
 
 // Setup Server
 const port = 3005;
