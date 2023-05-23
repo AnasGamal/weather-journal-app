098775e68173d3ef0c565b6fd7443d2fc68b4922
Anas Gamal <agamal@stetson.edu>
2023-05-23 05:45:38 +0300

    correct dependencies

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
-
+const path = require('path');
 // Start up an instance of app
 const app = express();
 // load .env file
