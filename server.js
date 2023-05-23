46b5ef803c58bcb141c47abf0b30dbae295f6131
Anas Gamal <agamal@stetson.edu>
2023-05-23 05:55:32 +0300

    missing port constant

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 // load .env file
 require('dotenv').config();
 
+const port = process.env.PORT || 3000;
 
 // Serve static files from the 'src' directory
 app.use(express.static(path.join(__dirname, 'src')));
